72506a0d624d44d47290de664cffa209d50dda6c
TreltaSev <treltasev@gmail.com>
2025-04-02 21:42:27 -0400

    Better Mongo Client

---
+++ backend/scripts/injectEvents.py
@@ -0,0 +1,52 @@
+"""
+From the mongo database, clears the events collection and 
+injects the events template from /templates/events
+"""
+
+import os
+import json
+from pathlib import Path
+from pyucc import console
+from siblink import Config
+from dotenv import dotenv_values
+from utils.mongo import MongoClient
+from utils.types.event import Event
+
+console.start("Injecting Events")
+
+# Load Customized Environment
+
+
+@Config.load_predetermined
+def _load_env():    
+    project_root: Path = Path(Config.root / '../')
+    Config.env = {
+        **dotenv_values(project_root / ".env"),
+        **dotenv_values(project_root / ".env.production.local")
+    }
+    os.environ.update(Config.env)  # Make sure os.gen_env works well
+
+
+_load_env()
+
+# Refresh & Load Client
+MongoClient.refresh_client()
+
+# Clear Collection
+MongoClient.events.delete_many({})
+
+console.done("Cleared Events Collection")
+
+# Load Events Template
+pTemplatesDirectory = Path(Config.root / "../templates/")
+pEventsTemplate = pTemplatesDirectory / "events-template.json"
+
+# Parse events template
+events = [Event(event).sanitized() for event in json.loads(pEventsTemplate.read_text())]
+
+console.done("Parsed Events Template")
+
+# Insert all events
+MongoClient.events.insert_many(events)
+
+console.done("Wrote Events to Collection")
--- backend/utils/mongo/MongoClient.py
+++ backend/utils/mongo/MongoClient.py
@@ -4,11 +4,30 @@ import pymongo.collection
 import pymongo.database
 from pyucc import console
 
+
 class MongoClient:
     client: pymongo.MongoClient = pymongo.MongoClient(os.getenv("MONGO_URL") or "mongodb://localhost:27017/")
     database: pymongo.database.Database = client["eday"]
-    
+
     # Collections
     logs: pymongo.collection.Collection = database["logs"]
     users: pymongo.collection.Collection = database["users"]
     events: pymongo.collection.Collection = database["events"]
+    
+    @classmethod
+    def refresh_client(cls):
+        
+        # Refresh client and database
+        cls.client = pymongo.MongoClient(os.getenv("MONGO_URL") or "mongodb://localhost:27017/")
+        cls.database = cls.client["eday"]
+        
+        # Clear existing collections
+        existing = set(cls.__dict__.keys())
+        for attr in existing:
+            if isinstance(getattr(cls, attr, None), pymongo.collection.Collection):
+                delattr(cls, attr)
+        
+        # Dynamically Assign Collections
+        for collection in cls.database.list_collection_names():
+            setattr(cls, collection, cls.database[collection])
+
