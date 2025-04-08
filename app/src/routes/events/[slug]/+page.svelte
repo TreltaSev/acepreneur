76765c72bb27047bb968af87d03034e3ff96f4fa
Trelta Sebastion <treltasev@gmail.com>
2025-04-08 15:06:24 -0400

    setup(event-slug): Setup basic slug logic

---
+++ app/src/routes/events/[slug]/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	// --- Components ---
+
+	// --- Logic ---
+	import { onMount } from 'svelte';
+	import { getIdentityCtx } from '@root/lib/ctx';
+
+    // Slug data object
+    import { page } from "$app/state";
+    const slug = page.params.slug
+
+	// Gather user object from the identity context
+    const { user } = getIdentityCtx()
+
+	// Reactive State to store event information
+
+	// Fetch the specified event on load
+	onMount(async () => {
+        // const event = await user.get_event(slug)
+
+    });
+</script>
+
+
+<span>event ;)</span>
