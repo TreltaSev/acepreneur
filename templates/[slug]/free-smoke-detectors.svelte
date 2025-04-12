b754acaba72c5fb7e8f085dd17dd2351d27725e3
TreltaSev <treltasev@gmail.com>
2025-04-12 10:58:39 -0400

    added/ more events

---
+++ templates/[slug]/free-smoke-detectors.svelte
@@ -0,0 +1,7 @@
+<!--@metadata
+name="Free Smoke Detectors"
+description="Sign up for free smoke detectors through the Firefighting class"
+slug="free-smoke-detectors"
+card.color="#cccccc"
+card.image.url="https://res.cloudinary.com/dtuexxqi2/image/upload/v1744468891/Smoke_detector_xmajl8.jpg"
+-->
---
+++ templates/[slug]/herbal-remedies-information.svelte
@@ -0,0 +1,7 @@
+<!--@metadata
+name="Herbal Remedies Information"
+description="Learn how common herbs and flowers can treat specific ailments with informational flyers from the Pharmacy program."
+slug="herbal-remedies"
+card.color="#c0c0b9"
+card.image.url="https://res.cloudinary.com/dtuexxqi2/image/upload/v1744468792/HerbalMedicine_share_jdfgza.jpg"
+-->
