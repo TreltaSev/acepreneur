935c1b9870203ba5df8b06ead5b65c108e575dd6
TreltaSev <treltasev@gmail.com>
2025-04-12 17:12:21 -0400

    new(component): Admin Only Component

--- app/src/lib/components/index.ts
+++ app/src/lib/components/index.ts
@@ -1,3 +1,4 @@
+export { default as AdminOnly } from "./admin-only/components/admin-only.svelte"
 export { default as Button } from "./button/components/button.svelte"
 export * as Navigation from "./navigation"
 export { default as SpeedDial } from "./speedial/speedial.svelte"
