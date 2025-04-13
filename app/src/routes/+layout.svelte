e6e85acca381dfb1f81e366ad4ab0792354cc634
TreltaSev <treltasev@gmail.com>
2025-04-13 19:07:13 -0400

    pwa(update)

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -133,6 +133,8 @@
 	<meta name="mobile-web-app-capable" content="yes" />
 	<meta name="apple-mobile-web-app-capable" content="yes" />
 	<link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+	<meta name="apple-mobile-web-app-status-bar-style" content="default" />
+	<link rel="apple-touch-icon" href="/icons/icon-192.png" />
 </svelte:head>
 
 <!-- App container with full height and white background -->
