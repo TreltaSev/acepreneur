94838d2e014ff03310505e28ade47de2d2b53c1b
TreltaSev <treltasev@gmail.com>
2025-04-15 17:30:29 -0400

    update(pricemap): Better did scaling

--- app/src/routes/(alt)/pricemap/+page.svelte
+++ app/src/routes/(alt)/pricemap/+page.svelte
@@ -2,6 +2,16 @@
 	import 'pinch-zoom-element';
 	import { onMount } from 'svelte';
 	let pinchZoom: undefined | any;
+
+	onMount(() => {
+		// The docs are so scuffed bro, had to use the source code :sob:
+		pinchZoom.scaleTo(0.9, {
+			originX: '50%',
+			originY: '30%',
+			relativeTo: 'container',
+			allowChangeEvent: true
+		});
+	});
 </script>
 
 <pinch-zoom bind:this={pinchZoom}>
@@ -9,10 +19,15 @@
 		<img
 			src="https://core-docs.s3.amazonaws.com/henrico_county_public_schools_ar/picture/data/774085/content_7DA_-4IwbOH4l_lkasPTfw.jpeg"
 			alt="a"
+			class="rounded-2xl"
 		/>
 
-		<h3 class="absolute -bottom-30 left-0">Pinch to move<br/> <a class="text-app" href="https://entrepreneurship-day.github.io/index.html">Visit the website</a></h3>
-        
+		<h3 class="absolute -bottom-30 left-0">
+			Pinch to move<br />
+			<a class="text-app" href="https://entrepreneurship-day.github.io/index.html"
+				>Click here to visit the website</a
+			>
+		</h3>
 	</div>
 </pinch-zoom>
 
