0ec192ed40e4f4ebc7d2fde70d4bacb2b8475726
dhanani-git-trial <dhanani.githubtrial@gmail.com>
2025-04-16 04:07:50 +0000

    move mother's day ---- event unknown at moment into ignore folder, started ace t-shirt thing

--- templates/event/[slug]/ace-t-shirt.svelte
+++ templates/event/[slug]/ace-t-shirt.svelte
@@ -1,9 +1,9 @@
 <!--@metadata
 name="Ace T-Shirt Collection"
-description="This is a description"
+description="Ace T-Shirt Collection is your go-to destination for bold, stylish, and meaningful tees that stand out—created with passion and creativity by the Graphic Communication students at the ACE Center at Hermitage. "
 slug="ace-t-shirt"
 card.color="#DD8E43"
-card.image.url="https://res.cloudinary.com/dtuexxqi2/image/upload/v1744368976/food-combos_sqs5ro.png"
+card.image.url="https://res.cloudinary.com/dlzshfcd1/image/upload/v1744767058/ACE_Shirt_Coll_or5tht.png"
 program="graphic-communications"
 -->
 
