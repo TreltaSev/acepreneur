bbcd06924859514b2bf174c8435367a8664691e9
Trelta Sebastion <treltasev@gmail.com>
2025-04-16 10:19:06 -0400

    update commercial photography

--- templates/event/[slug]/commercial-photography.svelte
+++ templates/event/[slug]/commercial-photography.svelte
@@ -7,10 +7,19 @@ card.image.url="https://res.cloudinary.com/dtuexxqi2/image/upload/v1744469580/ni
 -->
 
 <section>
-    <h3>Want a Headshot?</h3>
-    <h4>
-        You can register here to get your own very headshot, digital delivery is available :)
-    </h4>
-    <a href="https://docs.google.com/forms/d/e/1FAIpQLSfSJMrL-jJG9JQPMuG0yCdWkqcDxWWaIKots4-k809V3y0uCw/viewform"><button>Register</button></a>
-  </section>
-  
+  <h3>Want a Headshot?</h3>
+  <h4>
+    You can register here to get your own very headshot, digital delivery is
+    available :)
+  </h4>
+  <a
+    href="https://docs.google.com/forms/d/e/1FAIpQLSfSJMrL-jJG9JQPMuG0yCdWkqcDxWWaIKots4-k809V3y0uCw/viewform"
+    ><button>Register</button></a
+  >
+</section>
+
+<img
+  src="https://res.cloudinary.com/dtuexxqi2/image/upload/v1744813070/Screenshot_2025-04-16_101700_vhhffg.png"
+  style="width: 100%; object-fit: cover; border-radius: 16px; margin-top:20px;"
+  alt="ima"
+/>
