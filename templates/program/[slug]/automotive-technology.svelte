8f41c4f56eebd4369f217e68e9e4e9da30a8b79c
dhanani-git-trial <dhanani.githubtrial@gmail.com>
2025-04-16 15:10:16 +0000

    remove oil-changes

--- templates/program/[slug]/automotive-technology.svelte
+++ templates/program/[slug]/automotive-technology.svelte
@@ -2,7 +2,6 @@
 name="Automotive Technology"
 description="Prepares students to explore, handle and perform basic functions in engine repair, automatic transmission and transaxle, manual drive train and axles, suspension and steering systems, and brakes."
 slug="automotive-technology"
-events="oil-changes"
 card.color="#f4a261"
 card.image.url="https://res.cloudinary.com/dlzshfcd1/image/upload/v1744643606/AutoTech_ci13sb.png"
 -->
