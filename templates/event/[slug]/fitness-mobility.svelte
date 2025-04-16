8bfc30d5471aec5c888bb6ca690d8e0aec300d48
Trelta Sebastion <treltasev@gmail.com>
2025-04-16 10:14:53 -0400

    remove(fitness-mobility)

--- templates/event/[slug]/fitness-mobility.svelte
+++
@@ -1,7 +0,0 @@
-<!--@metadata
-name="Fitness & Mobility Station"
-description="Refreshing smoothies, reaction time drills for improving agility, chair squats for strength and mobility, and a comprehensive one-pager with a variety of effective chair exercises to enhance fitness and flexibility."
-slug="fitness-mobility"
-card.color="#4CAF50"
-card.image.url="https://res.cloudinary.com/dtuexxqi2/image/upload/v1744469580/nikon-d810_yc8b7f.jpg"
--->
